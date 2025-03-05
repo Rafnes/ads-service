11afaf7eaa8bc18fc537929dc641af878fe30444
gface <89259112970@mail.ru>
2025-03-05 23:51:00 +0300

    add: CommentDTO

--- src/main/java/ru/skypro/homework/dto/CommentDTO.java
+++ src/main/java/ru/skypro/homework/dto/CommentDTO.java
@@ -1,4 +1,10 @@
 package ru.skypro.homework.dto;
 
 public class CommentDTO {
+    private Integer author;
+    private String authorImage;
+    private String authorFirstName;
+    private Long createdAt;
+    private Integer pk;
+    private String text;
 }
