3f04bb9741e3d45aff5597f4880e4a92d6eca720
Fet2113 <super.gjoll@yandex.ru>
2025-03-13 01:35:58 +0300

    add ДОбавлена зависимость MapStruct в pom.xml. Добавлены AdMapper, UserMapper, CommentMapper.

---
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -0,0 +1,33 @@
+package ru.skypro.homework.mapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.skypro.homework.dto.CommentDTO;
+import ru.skypro.homework.dto.CommentsDTO;
+import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
+import ru.skypro.homework.model.Comment;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(source = "author", target = "author.id")
+    @Mapping(source = "pk", target = "ad.id")
+    @Mapping(source = "text", target = "text")
+    Comment toModel(CommentDTO dto);
+
+    @Mapping(source = "text", target = "text")
+    Comment toModel(CreateOrUpdateCommentDTO dto);
+
+    //_____ toDto___
+    @Mapping(source = "author.id", target = "author")
+    @Mapping(source = "ad.id", target = "pk")
+    @Mapping(source = "text", target = "text")
+    CommentDTO toDtoCommentDTO(Comment commentModel);
+
+    @Mapping(source = "text", target = "text")
+    CreateOrUpdateCommentDTO toDtoCreateOrUpdateCommentDTO(Comment commentModel);
+    @Mapping(target = "count", source = "size")
+    @Mapping(target = "results", source = "list")
+    CommentsDTO toDtoCommentsDTO(Integer size, List<Comment> list);
+}
