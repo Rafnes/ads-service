eb550f8ac3b4f4b895270a3b06e3327f544fa686
gface <89259112970@mail.ru>
2025-03-12 11:57:18 +0300

    fix: Comment, User v1.1

--- src/main/java/ru/skypro/homework/model/Comment.java
+++ src/main/java/ru/skypro/homework/model/Comment.java
@@ -1,6 +1,8 @@
 package ru.skypro.homework.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,8 @@ import javax.persistence.*;
 @Table(name = "comments")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
