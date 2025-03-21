b7bc49bbafca3056837eb9e4096e56d22055b3f7
bogomolova <ibogomolllova@gmail.com>
2025-03-21 20:25:38 +0200

    add: Image, ImageRepository, AdNotFoundException, UserNotFoundException, methods in ImageForAdServiceImpl, changeSet in changelog.yml, OneToOne & Join

---
+++ src/main/java/ru/skypro/homework/model/Image.java
@@ -0,0 +1,71 @@
+package ru.skypro.homework.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String filePath;
+
+    private long fileSize;
+
+    private String mediaType;
+
+    private byte[] savesDataInDb;
+
+    @OneToOne
+    @JoinColumn(name = "User_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "Ad_id")
+    private Ad ad;
+
+    public Image() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return id == image.id
+                && fileSize == image.fileSize
+                && Objects.equals(filePath, image.filePath)
+                && Objects.equals(mediaType, image.mediaType)
+                && Arrays.equals(savesDataInDb, image.savesDataInDb)
+                && Objects.equals(user, image.user)
+                && Objects.equals(ad, image.ad);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, filePath, fileSize, mediaType, user, ad);
+        result = 31 * result + Arrays.hashCode(savesDataInDb);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", filePath='" + filePath + '\'' +
+                ", fileSize=" + fileSize +
+                ", mediaType='" + mediaType + '\'' +
+                ", savesDataInDb=" + Arrays.toString(savesDataInDb) +
+                ", user=" + user +
+                ", ad=" + ad +
+                '}';
+    }
+}
